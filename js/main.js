3fcb232cdbedd706d9a1fa3b893b90736ef65dc2
vanita22 <vanitayrafita2004@gmail.com>
2017-06-16 00:34:52 -0300

    subiendo trabajo casi completado

--- js/main.js
+++ js/main.js
@@ -11,6 +11,26 @@ function permitirDrop(ev){
 function drop(ev){
 	ev.preventDefault();
 	var menu = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(menu));
+	ev.target.appendChild(document.getElementById(menu));/*valiadacion*/
 }
 
+/*valiadacion*/
+
+function validacion(){
+	var name = document.getElementById('nombre').value;
+	var password = document.getElementById('password').value;
+
+	if(name == " " && name == 'number'){
+		alert("ingrese nombre");
+		return false;
+	}
+
+	if(password == " " && password < 6){
+		alert("ingrese contraseña con letras y números mayor a 6 carácteres");
+		return false;
+	}
+
+	return true;
+}
+
+validacion();
